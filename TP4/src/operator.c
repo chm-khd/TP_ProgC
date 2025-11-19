c29a74670b5b16c1060a7e2646c6a578d775f1ce
chm-khd <chaimakhd19@gmail.com>
2025-11-19 14:52:02 +0100

    Update operator.c

---
+++ TP4/src/operator.c
@@ -0,0 +1,45 @@
+#include "operator.h"
+#include <stdio.h>
+
+
+int somme(int a, int b) {
+    return a + b;
+}
+
+int difference(int a, int b) {
+    return a - b;
+}
+
+int produit(int a, int b) {
+    return a * b;
+}
+
+int quotient(int a, int b) {
+    if (b != 0)
+        return a / b;
+    else {
+        printf("Erreur : division par zéro !\n");
+        return 0;
+    }
+}
+
+int modulo(int a, int b) {
+    if (b != 0)
+        return a % b;
+    else {
+        printf("Erreur : modulo par zéro !\n");
+        return 0;
+    }
+}
+
+int et(int a, int b) {
+    return a & b;
+}
+
+int ou(int a, int b) {
+    return a | b;
+}
+
+int negation(int a) {
+    return ~a;
+}
