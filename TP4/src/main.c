c6419dfaf2eabea04db7d5edbce90eeca49b1f77
chm-khd <chaimakhd19@gmail.com>
2025-11-19 14:52:35 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include "operator.h"
+
+int main() {
+    int num1, num2, resultat;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+    printf("Entrez l'opérateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op); 
+
+    switch(op) {
+        case '+':
+            resultat = somme(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '-':
+            resultat = difference(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '*':
+            resultat = produit(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '/':
+            resultat = quotient(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '%':
+            resultat = modulo(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '&':
+            resultat = et(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '|':
+            resultat = ou(num1, num2);
+            printf("Résultat : %d\n", resultat);
+            break;
+        case '~':
+            resultat = negation(num1);
+            printf("Résultat : %d\n", resultat);
+            break;
+        default:
+            printf("Opérateur invalide !\n");
+            break;
+    }
+
+    return 0;
+}
 
